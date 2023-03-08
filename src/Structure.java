0a6048b3e489cde2d174c3847ae11623ea23e57c
olg <o.olpinski@gmail.com>
2023-03-08 22:53:36 +0100

    interpretacja interfejsu Structure

--- src/Structure.java
+++ src/Structure.java
@@ -2,6 +2,10 @@ import java.util.List;
 import java.util.Optional;
 
 public interface Structure {
+    /*klasa implementująca ten interfejs będzie musiała zawierać w sobie kolekcję obiektów klasy implementującej interfejs Block
+        Czyli struktura (obiekt klasy implementującej interfejs Structure) będzię zbiorem wielu bloków,
+        wynika to z poniższych metod które mają zwrócić informacje o blokach zawartych w tej strukturze
+    * */
 
     Optional<Block> findBlockByColor(String color);
     List<Block> findBlocksByMaterial(String material);
